06208cf22958a633e8c6d6adef7b840b64a4d7d8
karanjalight <karanjalight@gmail.com>
2022-08-26 00:21:45 +0300

    bored

---
+++ frontend/build/precache-manifest.834ee1fedca77f1088a8e676536a45e5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a80f4e51e7a50ced46b9f962f362a9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ab916d3f4e9c390679e",
+    "url": "/static/css/main.94c8128e.chunk.css"
+  },
+  {
+    "revision": "ab66e5deb41f5869ed51",
+    "url": "/static/js/2.4fbd73ac.chunk.js"
+  },
+  {
+    "revision": "7ab916d3f4e9c390679e",
+    "url": "/static/js/main.e96bcd63.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "346af2450c58ec9d89cb6b3a87bcc984",
+    "url": "/static/media/cover.346af245.jpg"
+  },
+  {
+    "revision": "fc4438952a3c8b00f74158b7a7887f2f",
+    "url": "/static/media/star-black.fc443895.png"
+  },
+  {
+    "revision": "3a0dda2d866e65c49af532492c4ca27f",
+    "url": "/static/media/star.3a0dda2d.png"
+  }
+]);
